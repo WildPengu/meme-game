390a5042fca2e7dd80a0accd2a52f7d22c93e0da
WildPengu <7241679@wp.pl>
2020-04-06 18:33:00 +0200

    Updates

---
+++ precache-manifest.0e8465cca73f66ad4fc4d078cbaf2072.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0189cc91563f417118aa47360266921d",
+    "url": "/meme-game/index.html"
+  },
+  {
+    "revision": "6cbd499bdb16140aa74f",
+    "url": "/meme-game/static/css/main.d3c522ef.chunk.css"
+  },
+  {
+    "revision": "fb32f7a97a215f10a1b0",
+    "url": "/meme-game/static/js/2.89cd0988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-game/static/js/2.89cd0988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbd499bdb16140aa74f",
+    "url": "/meme-game/static/js/main.ccce83d6.chunk.js"
+  },
+  {
+    "revision": "3515c0101b2819305cdf",
+    "url": "/meme-game/static/js/runtime-main.bec51d90.js"
+  },
+  {
+    "revision": "d9682af1ea94e29458df5d1fd5893313",
+    "url": "/meme-game/static/media/5.d9682af1.jpg"
+  },
+  {
+    "revision": "88d348d7da427b1ade07f0ac2d474a13",
+    "url": "/meme-game/static/media/angrypepe.88d348d7.PNG"
+  },
+  {
+    "revision": "2428da8d691d053a026163bf1f6a8c96",
+    "url": "/meme-game/static/media/beeHappy.2428da8d.PNG"
+  },
+  {
+    "revision": "33cf5a77a106631eb6fa3caf894eef49",
+    "url": "/meme-game/static/media/dabPengu.33cf5a77.PNG"
+  },
+  {
+    "revision": "c91883191aac9bb8a9f852e04a9bf84f",
+    "url": "/meme-game/static/media/eca.c9188319.jpg"
+  },
+  {
+    "revision": "a2af81f20b77929704698152bafc26cd",
+    "url": "/meme-game/static/media/gearsIcon.a2af81f2.png"
+  },
+  {
+    "revision": "c49704e49621f8e00029c8c49bf1382e",
+    "url": "/meme-game/static/media/happyCat.c49704e4.PNG"
+  },
+  {
+    "revision": "3eb11dfec22fb496d6007197ea94ef31",
+    "url": "/meme-game/static/media/happyPengu.3eb11dfe.PNG"
+  },
+  {
+    "revision": "fcd25223fd548d4cc959dff619e849a3",
+    "url": "/meme-game/static/media/iLoveIt.fcd25223.PNG"
+  },
+  {
+    "revision": "2f70fb94b142dadc383d7f8d06af1480",
+    "url": "/meme-game/static/media/jinx.2f70fb94.PNG"
+  },
+  {
+    "revision": "b75dbbe0ce745b8a91457b442bae59d5",
+    "url": "/meme-game/static/media/joyPengu.b75dbbe0.PNG"
+  },
+  {
+    "revision": "095aefd48afdcfa629bcc0a916cd6989",
+    "url": "/meme-game/static/media/meow.095aefd4.PNG"
+  },
+  {
+    "revision": "4d035e7a8fcf7daf62dd50c557039a64",
+    "url": "/meme-game/static/media/muffin.4d035e7a.jpg"
+  },
+  {
+    "revision": "6989678130e6ba72f121fe4a56f093a4",
+    "url": "/meme-game/static/media/perfect.69896781.PNG"
+  },
+  {
+    "revision": "28e64c5ba5ba4449d8581610af3503f8",
+    "url": "/meme-game/static/media/pozytywnyOpos.28e64c5b.jpg"
+  },
+  {
+    "revision": "8bbbac1ea2494fc41860cbad89920e47",
+    "url": "/meme-game/static/media/rammus.8bbbac1e.PNG"
+  },
+  {
+    "revision": "cf8d88bb05b8bc707ca148d55c9779ea",
+    "url": "/meme-game/static/media/sadKitten.cf8d88bb.PNG"
+  }
+]);
